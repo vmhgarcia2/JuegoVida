d6e7cedf9ce9d16323139258569b522372b420ab
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-10 13:36:56 +0200

    [RED] Test celula creados

---
+++ Juego/Celula.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juego
+{
+    public class Celula
+    {
+        //Estado: true=> viva
+        private bool estado;
+
+        public bool Estado
+        {
+            get => estado;
+            set => estado = value;
+        }
+
+        public static bool SigueViva(bool estado, byte cantidad)
+        {
+            throw new Exception("No implementado");
+        }
+
+    }
+}
--- TestJuego/UnitTest1.cs
+++ TestJuego/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Juego;
 
 namespace Tests
 {
@@ -10,9 +11,229 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void TestRevive()
         {
-            Assert.Pass();
+            //Arrange
+            bool estadoActual = false;
+            byte num = 3;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsTrue(estado);
+        }
+
+        [Test]
+        public void TestSigueViva2()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 2;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsTrue(estado);
+        }
+
+        [Test]
+        public void TestSigueViva3()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 3;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsTrue(estado);
+        }
+
+        [Test]
+        public void TestMuere1()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 1;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestMuere4()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 4;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestMuere5()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 5;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestMuere6()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 6;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestMuere7()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 7;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestMuere8()
+        {
+            //Arrange
+            bool estadoActual = true;
+            byte num = 8;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+
+        [Test]
+        public void TestSigueMuerta1()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 1;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestSigueMuerta4()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 4;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestSigueMuerta5()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 5;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestSigueMuerta6()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 6;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestSigueMuerta7()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 7;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
+        }
+
+        [Test]
+        public void TestSigueMuerta8()
+        {
+            //Arrange
+            bool estadoActual = false;
+            byte num = 8;
+            bool estado;
+
+            //Act
+            estado = Celula.SigueViva(estadoActual, num);
+
+            //Assert
+            Assert.IsFalse(estado);
         }
     }
 }
